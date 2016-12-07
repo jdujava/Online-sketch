01a8764c52d9edb5ddae0f1879963d7290747747
jdujava <jonas.dujava1@gmail.com>
2016-12-07 18:17:10 +0100

    fixed position of peopleCounter

--- public/sketch.js
+++ public/sketch.js
@@ -1,9 +1,9 @@
 var socket;
+var p = getElementById('peopleCounter');
 
 function setup() {
   createCanvas(1000,800);
   background(51);
-  var p = createElement('h1', 'Počet pripojených ľudí : 0');
 
   socket = io.connect('https://whispering-shelf-36846.herokuapp.com/');
   socket.on('mouse', newDrawing);
