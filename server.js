c4b03615481098ef799536afbb1d707988384ffa
jdujava <jonas.dujava1@gmail.com>
2016-12-07 16:41:50 +0100

    fixed server.js

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var express = require('express');
 
 var app = express();
 
-app.set('port', (5000));
-var server = app.listen(5000);
+app.set('port', (process.env.PORT || 5000));
+var server = app.listen(process.env.PORT || 5000);
 
 app.use(express.static(__dirname + '/public'));
 
