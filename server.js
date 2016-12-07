cc1c5eb41800fa26f60e44d046bbdf70412fbc37
jdujava <jonas.dujava1@gmail.com>
2016-12-07 16:13:14 +0100

    changed server.js

--- server.js
+++ server.js
@@ -2,11 +2,11 @@
 var express = require('express');
 
 var app = express();
-var server = app.listen(3000);
 
-app.use(express.static('public'));
+app.set('port', (process.env.PORT || 5000));
 
-console.log("Server is running");
+app.use(express.static(__dirname + '/public'));
+
 
 var socket = require('socket.io');
 var io = socket(server);
